97dae22be841b5ec38c2ded8e4471443d8a7e532
Allen9478 <allenyu0708@gmail.com>
2026-04-04 23:52:58 +0800

    learn vue until watch

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,90 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref , computed , watch} from 'vue'
+
+const todos = ref(JSON.parse(localStorage.getItem('todos')) || [
+  { id: 1, title: '吃飯', isDone: false },
+  { id: 2, title: '喝水', isDone: false },
+  { id: 3, title: '睡覺', isDone: false }
+])
+
+const addTodoInput = ref('')
+
+const addTodoFn = () => {
+  if (addTodoInput.value.trim() === '') {
+    alert('請輸入代辦事項')
+    return
+  }
+  const newTodo = {
+    id: Date.now(),
+    title: addTodoInput.value,
+    isDone: false
+  }
+  
+  todos.value.push(newTodo)
+  addTodoInput.value = ''  
+}
+
+// ✅ 正確：接收 id，用 filter 移除對應項目
+const delTodoFn = (id) => {
+  todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+const toggleTodo = (id) => {
+  const target = todos.value.find(todo => todo.id === id)
+  if (!target) return  // 防禦性寫法，找不到就離開
+  target.isDone = !target.isDone
+}
+
+const finishedCount = computed(() => 
+  todos.value.filter(todo => todo.isDone).length
+)
+
+const unfinishedCount = computed(() => 
+  todos.value.filter(todo => !todo.isDone).length
+)
+
+watch(todos, (newVal) => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, {deep:true})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <ul>
+     <li v-for="todo in todos" :key="todo.id">
+      <span :class=" { done: todo.isDone} "> {{ todo.title }} </span> 
+      <button @click="toggleTodo(todo.id)">
+        {{ todo.isDone ? "點擊取消" : "點擊完成" }}
+      </button>
+      <button @click="delTodoFn(todo.id)">刪除</button>
+     </li>
+    </ul>
+    <div >
+      <input type="text" v-model="addTodoInput">
+      <button @click="addTodoFn">新增</button>
+       <p>已完成數量：{{ finishedCount }}</p>
+        <p>未完成數量：{{ unfinishedCount }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  font-family: sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+button {
+  padding: 8px 24px;
+  font-size: 16px;
+  cursor: pointer;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+  text-decoration: line-through;
+  color: #aaa;
 }
 </style>
